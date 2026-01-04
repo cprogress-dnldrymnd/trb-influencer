6de7f580cecf9d68c8e725b4e67fda9825ba9708
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-05 01:13:04 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -25,14 +25,8 @@ define('HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0');
 function hello_elementor_child_scripts_styles()
 {
 
-    wp_enqueue_style(
-        'hello-elementor-child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        [
-            'hello-elementor-theme-style',
-        ],
-        HELLO_ELEMENTOR_CHILD_VERSION
-    );
+    wp_enqueue_style('hello-elementor-child-style', get_stylesheet_directory_uri() . '/style.css', ['hello-elementor-theme-style',], HELLO_ELEMENTOR_CHILD_VERSION);
+    wp_enqueue_script('helle-elementor-main-js', get_stylesheet_directory_uri() . '/assets/js/main.js', ['jquery'], HELLO_ELEMENTOR_CHILD_VERSION, true);
 }
 add_action('wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20);
 
