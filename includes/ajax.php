7898dbc203d68d56d415918585a513c6efce0fd6
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-05 04:41:09 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -96,13 +96,16 @@ function my_custom_loop_filter_handler()
 
     // 3. EXECUTE QUERY
     $query = new WP_Query($args);
+        ob_start();
 
     // 4. RENDER ELEMENTOR LOOP
-    if ($query->have_posts()) {
-        ob_start();
-        echo '<pre>';
+     echo '<pre>';
         var_dump($args);
         echo '</pre>';
+        wp_send_json_success(ob_get_clean());
+
+    if ($query->have_posts()) {
+       
         while ($query->have_posts()) {
             $query->the_post();
             if (class_exists('\Elementor\Plugin')) {
@@ -110,7 +113,6 @@ function my_custom_loop_filter_handler()
             }
         }
         wp_reset_postdata();
-        wp_send_json_success(ob_get_clean());
     } else {
         wp_send_json_error('No posts found');
     }
