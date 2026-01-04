d9c318227aab712d5565a1f895aa73dba8569625
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-05 00:49:28 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -39,6 +39,7 @@ add_action('wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20);
 include 'includes/pmpro.php';
 include 'includes/custom-functions.php';
 include 'includes/elementor.php';
+include 'includes/shortcodes.php';
 
 
 /**
