fb519ca564a5bc9e8a7a83d0d7675b1d0be72458
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-05 01:26:01 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,6 @@ function nicheToggle() {
     jQuery('.niche-toggle').click(function (e) {
         e.preventDefault();
         jQuery(this).parent().find('.niche-term').show();
+        jQuery(this).hide();
     });
 }
