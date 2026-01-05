9f8a41303af4ffe80f07e84b735cc1991df2db65
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-06 00:25:33 +0800

    Update single-influencer.php

--- single-influencer.php
+++ single-influencer.php
@@ -1,17 +1,8 @@
 <?php get_header() ?>
 <?php while(have_posts()) { ?>
 <?php the_post(); ?>
-<div class="dashboard">
-    <div class="dashboard-header">
-        <?= do_shortcode('[elementor-template id="1571"]') ?>
-    </div>
-    <div class="dashboard-content">
-        <?= do_shortcode('[elementor-template id="1640"]') ?>
-        <?php // do_shortcode('[elementor-template id="1868"]') ?> 
-
         <?= do_shortcode('[mycred_sell_this] [elementor-template id="2442"] [/mycred_sell_this]') ?>
-    </div>
-</div>
+
 <?php }?>
 <?php get_footer() ?>
 
