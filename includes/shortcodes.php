89bd3484baa2eb8b656781b891ecc9630cd562d9
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-05 17:09:58 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -224,7 +224,7 @@ function shortcode_influencer_search_filter()
                 <?= select_filter('niche', 'Tag Filter', $niche_options) ?>
             </div>
             <div class="influencer-search-item">
-                <?= checkbox_filter('platform', 'Platform', $platform) ?>
+                <?= checkbox_filter('platform', 'Platform', $platform_options) ?>
             </div>
 
             <div class="influencer-search-item">
