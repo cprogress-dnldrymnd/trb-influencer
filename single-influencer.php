82a926cc490d197b83fa0fd0311019ce930f833b
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-06 00:25:15 +0800

    Update single-influencer.php

--- single-influencer.php
+++ single-influencer.php
@@ -7,7 +7,7 @@
     </div>
     <div class="dashboard-content">
         <?= do_shortcode('[elementor-template id="1640"]') ?>
-        <?php// do_shortcode('[elementor-template id="1868"]') ?> 
+        <?php // do_shortcode('[elementor-template id="1868"]') ?> 
 
         <?= do_shortcode('[mycred_sell_this] [elementor-template id="2442"] [/mycred_sell_this]') ?>
     </div>
