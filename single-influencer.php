e130aaef6d1b1f696c2ed70a47b56e596ad76420
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-06 00:32:21 +0800

    Update single-influencer.php

--- single-influencer.php
+++ single-influencer.php
@@ -2,7 +2,13 @@
 <?php while(have_posts()) { ?>
 <?php the_post(); ?>
 <div class="dashboard">
-   <?= do_shortcode('[elementor-template id="2483"]') ?> 
+    <div class="dashboard-header">
+        <?= do_shortcode('[elementor-template id="1571"]') ?>
+    </div>
+    <div class="dashboard-content">
+        <?= do_shortcode('[elementor-template id="1640"]') ?>
+        <?= do_shortcode('[elementor-template id="1868"]') ?> 
+    </div>
 </div>
 <?php }?>
 <?php get_footer() ?>
