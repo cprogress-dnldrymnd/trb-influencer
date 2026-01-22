9d3704fd58c74f2b512df949913c2f1b10e88815
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-22 21:57:59 +0800

    css

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -496,7 +496,7 @@ function custom_avatar_dropdown_shortcode($atts) {
     $output = "
     <div class='cad-wrapper' onclick='this.classList.toggle(\"active\")'>
         <div class='cad-trigger'>
-            <img src='{$avatar_url}' alt='User Avatar' class='cad-avatar'>
+            <div class='cad-avatar-wrapper'><img src='{$avatar_url}' alt='User Avatar' class='cad-avatar'></div>
             <span class='cad-arrow'>
                 <svg width='10' height='6' viewBox='0 0 10 6' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L5 5L9 1' stroke='#333' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/></svg>
             </span>
