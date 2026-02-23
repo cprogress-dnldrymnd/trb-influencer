fea32f43bf2e185eeccac93b8b5d3201144c33f5
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-02-23 12:28:24 +0800

    css

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -618,7 +618,9 @@ function shortcode_influencer_search_summary()
                     </svg> Your brief:
                 </div>
                 <div class="summary-brief">
-                    <?= wpautop(esc_html(wp_trim_words($brief, 25))) ?>
+                    <div class="summary-brief-inner">
+                        <?= wpautop(esc_html(wp_trim_words($brief, 25))) ?>
+                    </div>
                 </div>
                 <div class="edit-summary-brieft">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
