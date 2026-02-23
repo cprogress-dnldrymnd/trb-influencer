a48acaa648746d4fbb514726d5cd5126456c1939
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-02-23 16:33:00 +0800

    Update outreach.php

--- includes/outreach.php
+++ includes/outreach.php
@@ -506,6 +506,9 @@ class DD_Outreach_Manager
                 color: #BCBCBC;
                 border-color: #BCBCBC;
             }
+            .dd-note-btn.dd-note-btn.dd-note-btn.dd-note-btn-outline span {
+                text-decoration: underline;
+            }
         </style>
     <?php
     }
@@ -736,7 +739,7 @@ class DD_Outreach_Manager
                 $html .= '<button class="dd-delete-note dd-delete-btn dd-note-btn" data-post-id="' . esc_attr($post_id) . '" data-note-id="' . $note_id . '">DELETE NOTE</button>';
                 $html .= '<button class="dd-edit-note dd-edit-btn dd-note-btn dd-note-btn-outline" data-post-id="' . esc_attr($post_id) . '" data-note-id="' . $note_id . '">  <svg xmlns="http://www.w3.org/2000/svg" width="12.832" height="16.332" viewBox="0 0 12.832 16.332">
                             <path id="saved" fill="currentColor" d="M26.125,10.333V22a.583.583,0,0,1-.583.583h-.083a.584.584,0,0,1-.416-.174l-4.167-4.243-4.167,4.243a.583.583,0,0,1-.416.174h-.083A.583.583,0,0,1,15.625,22V10.333a1.752,1.752,0,0,1,1.75-1.75h7a1.752,1.752,0,0,1,1.75,1.75ZM25.541,6.25h-7a.583.583,0,0,0,0,1.167h7a1.752,1.752,0,0,1,1.75,1.75V18.5a.583.583,0,1,0,1.167,0V9.166A2.92,2.92,0,0,0,25.541,6.25Z" transform="translate(-15.625 -6.25)" />
-                        </svg> EDIT NOTE</button>';
+                        </svg> <span>EDIT NOTE</span></button>';
                 $html .= '</div>';
 
                 $html .= '<p class="dd-last-edited">Last edited ' . $fmt_date . '</p>';
