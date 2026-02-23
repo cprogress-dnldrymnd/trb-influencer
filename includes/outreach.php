5d32803970f5f71432595328e8bf41a773acce98
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-02-23 15:54:59 +0800

    Update outreach.php

--- includes/outreach.php
+++ includes/outreach.php
@@ -482,7 +482,7 @@ class DD_Outreach_Manager
             'post_type'   => 'outreach',
             'post_status' => 'publish',
             'post_author' => $current_user_id,
-            'post_content' => $data['message']
+            'post_content' => $data['message'].'<div class="hide-element">'.get_the_title($data['influencer_id']).'</div>'
         ];
 
         $post_id = wp_insert_post($new_post_args);
