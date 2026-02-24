11fcbfea994df02fbdc491fff1667cb9f5905a14
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-02-24 20:00:28 +0800

    css

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1709,7 +1709,9 @@ function shortcode_influencer_hashtags()
 
 
     $html = '<div class="influencer-hashtags">';
+    $html = '<div class="influencer-hashtags-title">';
     $html .= $count . ' NUMBER OF HASHTAGS';
+    $html .= '</div>';
     $html .= render_hashtag_cloud($hashtags);
     $html .= '</div>';
     return $html;
