f7435094f6ccaa01e3e4fc2aea011a1c74ffcdf8
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-02-27 17:35:49 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -695,7 +695,10 @@ function shortcode_influencer_search_summary()
                 </a>
             </div>
         <?php endif; ?>
-        <?php if (! empty($parts) && empty($brief)) : ?>
+        <?php
+        //&& empty($brief)
+        ?>
+        <?php if (! empty($parts) ) : ?>
             <div class="search-summary-item search-summary-filters">
                 <strong>Filters:</strong> <?= esc_html(implode(' • ', $parts)) ?>
             </div>
