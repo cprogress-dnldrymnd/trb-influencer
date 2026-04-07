a8e80e91bb2cfa940477f263ae9b6852e7ed9487
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-04-07 22:13:23 +0800

    Update pmpro.php

--- includes/pmpro.php
+++ includes/pmpro.php
@@ -581,6 +581,7 @@ function dd_influencer_style_pmpro_checkout()
         'default' => [
             'account_type' => '1 Account',
             'bullets' => [
+                'Enjoy unlimited access to your selected plan features.',
                 'From the starting date shown, you\'ll be charged for your updated subscription.',
                 'Cancel anytime online. <a href="/terms-of-use/">Terms apply</a>.'
             ]
