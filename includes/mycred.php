0f0cb56631b3f4b173d49a73fb128bfefd058994
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-04-07 22:30:48 +0800

    Update mycred.php

--- includes/mycred.php
+++ includes/mycred.php
@@ -218,7 +218,7 @@ function dd_influencer_style_mycred_checkout()
             color: inh;
         }
 
-        .mycred-stripe-payment-main * {
+        .mycred-stripe-payment-main.mycred-stripe-payment-main * {
             font-family: Inter;
             text-align: inherit;
         }
