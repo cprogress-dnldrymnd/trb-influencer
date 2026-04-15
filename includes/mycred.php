ac72f5f67b0bfcaff15e5ed53f4bd4eaafd96d61
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-04-15 23:07:15 +0800

    Update mycred.php

--- includes/mycred.php
+++ includes/mycred.php
@@ -118,7 +118,7 @@ function dd_influencer_style_mycred_checkout()
 ?>
     <style>
         /* Broadened selector to catch non-Stripe gateways */
-        .mycred-stripe-payment-main.mycred-stripe-payment-main {
+        .mycred-stripe-payment-main.mycred-stripe-payment-main *{
             font-family: Inter !important;
             color: inherit !important;
             text-align: left;
