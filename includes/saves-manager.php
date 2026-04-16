d84e4b36f064200cd8b863c5e6f64678f7c9fd0d
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-04-16 23:25:26 +0800

    Update saves-manager.php

--- includes/saves-manager.php
+++ includes/saves-manager.php
@@ -153,7 +153,7 @@ class Saves_Manager
                 <span class="elementor-button-content-wrapper">
                     <span class="elementor-button-icon">
                         <svg aria-hidden="true" class="e-font-icon-svg e-fas-bookmark" viewBox="0 0 384 512" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M0 512V48C0 21.49 21.49 0 48 0h288c26.51 0 48 21.49 48 48v464L192 400 0 512z"></path>
+                            <path fill="currentColor" d="M0 512V48C0 21.49 21.49 0 48 0h288c26.51 0 48 21.49 48 48v464L192 400 0 512z"></path>
                         </svg>
                     </span>
                     <span class="elementor-button-text"><?php echo esc_html($button_text); ?></span>
