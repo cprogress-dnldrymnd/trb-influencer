b9eb6a21c81aa53d4aec33513b50f32833bfb20e
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-04-20 21:16:54 +0800

    cs

--- includes/pmpro.php
+++ includes/pmpro.php
@@ -2,34 +2,6 @@
 if (! defined('ABSPATH')) {
     exit; // Exit if accessed directly to prevent direct file execution.
 }
-/**
- * Shortcode to display current user's PMPro membership level name.
- * Usage: [current_membership_level]
- */
-add_shortcode('current_membership_level', 'get_pmpro_membership_level_shortcode');
-
-function get_pmpro_membership_level_shortcode()
-{
-    // Ensure PMPro is active
-    if (! function_exists('pmpro_getMembershipLevelForUser')) {
-        return '';
-    }
-
-    $current_user_id = get_current_user_id();
-
-    // If user is not logged in, return early
-    if (empty($current_user_id)) {
-        return 'Guest';
-    }
-
-    $membership_level = pmpro_getMembershipLevelForUser($current_user_id);
-
-    if (! empty($membership_level)) {
-        return esc_html($membership_level->name);
-    }
-
-    return 'No Active Membership';
-}
 
 /**
  * Description: Automatically cancels all other active membership levels when a user obtains a new level, regardless of group association.
--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -2299,3 +2299,34 @@ function dd_current_year_shortcode()
 
 // Register the shortcode with WordPress
 add_shortcode('current_year', 'dd_current_year_shortcode');
+
+
+
+/**
+ * Shortcode to display current user's PMPro membership level name.
+ * Usage: [current_membership_level]
+ */
+add_shortcode('current_membership_level', 'get_pmpro_membership_level_shortcode');
+
+function get_pmpro_membership_level_shortcode()
+{
+    // Ensure PMPro is active
+    if (! function_exists('pmpro_getMembershipLevelForUser')) {
+        return '';
+    }
+
+    $current_user_id = get_current_user_id();
+
+    // If user is not logged in, return early
+    if (empty($current_user_id)) {
+        return 'Guest';
+    }
+
+    $membership_level = pmpro_getMembershipLevelForUser($current_user_id);
+
+    if (! empty($membership_level)) {
+        return esc_html($membership_level->name);
+    }
+
+    return 'No Active Membership';
+}
