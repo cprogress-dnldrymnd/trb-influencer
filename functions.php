d36bbcb043eba69d7730046d7821e9177d66a496
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-04-20 21:45:39 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -99,32 +99,6 @@ add_action('init', function () {
 
 
 /**
- * Remove custom trial for existing members (when existing member changes levels/renews)
- * * You can add this recipe to your site by creating a custom plugin
- * or using the Code Snippets plugin available for free in the WordPress repository.
- */
-function my_pmpro_level_adjustment( $level ) {
-
-    // Bail if the user currently doesn't have a membership level.
-    if ( ! pmpro_hasMembershipLevel() ) {
-        return $level;
-    }
-
-    // If it's not the level with the trial amount, just bail. Adjust the numeric value for the level ID that should be altered.
-    if ( $level->id != '9' || $level->id != '8' ) {
-        return $level;
-    }
-
-    $level->trial_limit  = '0';
-    $level->trial_amount = '0';
-
-    return $level;
-}
-add_filter( 'pmpro_checkout_level', 'my_pmpro_level_adjustment', 10, 1 );
-
-
-
-/**
  * Remove subscription delay for logged-in current or past members. 
  * EXCEPTION: Leaves the delay active for Level 15.
  */
