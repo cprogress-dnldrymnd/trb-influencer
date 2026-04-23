429f1146edddb42a8e0e6839288fdfa1b3518fed
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-04-23 18:35:45 +0800

    cs

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ $dir = get_stylesheet_directory();
 // Direct, unrolled require statements. 
 // This is the fastest execution path in PHP for procedural files.
 require $dir . '/includes/sign-up.php';
-require $dir . '/includes/influencers.php';
+//require $dir . '/includes/influencers.php';
 require $dir . '/includes/hooks.php';
 require $dir . '/includes/custom-functions.php';
 require $dir . '/includes/brief-parser.php';
