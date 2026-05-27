8481859f7194d7040a5612b590571f34421635f9
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-27 17:27:26 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -476,7 +476,7 @@ function shortcode_influencer_search_filter()
             </div>-->
 
             <div class="influencer-search-item">
-                <?= select_filter('min_followers', 'Maximum Followers', 'Select Maximum Followers', $influencer_search_fields['followers'] ?? '', 'radio') ?>
+                <?= select_filter('min_followers', 'Minimum Followers', 'Select Minimum Followers', $influencer_search_fields['followers'] ?? '', 'radio') ?>
             </div>
             <div class="influencer-search-item">
                 <?= select_filter('max_followers', 'Maximum Followers', 'Select Maximum Followers', $influencer_search_fields['followers'] ?? '', 'radio') ?>
