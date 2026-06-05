e7c4d881088827d7ec1b7880d48c0af00f4aab86
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-06-05 19:09:16 +0800

    saves-manager.php

--- modules/saves/saves-manager.php
+++ modules/saves/saves-manager.php
@@ -598,7 +598,7 @@ class Saves_Manager
     ?>
         <div class="inf-saved-groups-wrapper">
             <div class="inf-saved-groups-header">
-                <button class="inf-btn inf-shortcode-add-group">
+                <button class="inf-btn inf-shortcode-add-group" style="flex: unset !important">
                     <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" style="margin-right:6px;">
                         <line x1="12" y1="5" x2="12" y2="19"></line>
                         <line x1="5" y1="12" x2="19" y2="12"></line>
