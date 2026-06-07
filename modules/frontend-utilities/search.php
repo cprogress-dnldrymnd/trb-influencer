123f6703fe4634dbcf0dcf1351fbc23eab35bcd2
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-06-08 01:34:14 +0800

    style.css

--- modules/frontend-utilities/search.php
+++ modules/frontend-utilities/search.php
@@ -82,7 +82,7 @@ class Influencer_Search
             '10000000' => '10M',
         );
 
-          $max_followers_options = array(
+        $max_followers_options = array(
             '1000' => '1K',
             '10000' => '10K',
             '50000' => '50K',
@@ -1657,7 +1657,9 @@ class Influencer_Search
                         <?php if (! empty($refine_icon)) { ?>
                             <img class="refine-search-icon" src="<?= esc_url($refine_icon) ?>" alt="" />
                         <?php } else { ?>
-                            <svg class="refine-search-icon" xmlns="http://www.w3.org/2000/svg" width="23.66" height="20" viewBox="0 0 23.66 20"><path id="target" d="M24.044,20.152A10.187,10.187,0,0,1,24.1,21.2,10,10,0,1,1,19.973,13.1l-.745,2.778a7.375,7.375,0,1,0,2.037,3.527l2.777.744ZM13.436,21.579a.764.764,0,0,0,1.045.278l6.549-3.781,2.312.619,4.414-2.549-3.356-.9.9-3.356-4.414,2.549-.619,2.312-6.551,3.782a.764.764,0,0,0-.278,1.045Zm.661-3.032a2.671,2.671,0,0,1,.518.05L17.2,17.106a5.132,5.132,0,1,0,2.03,4.089,5.173,5.173,0,0,0-.04-.641l-2.582,1.491a2.649,2.649,0,1,1-2.51-3.5Z" transform="translate(-4.097 -11.195)" fill="#00a6ed" fill-rule="evenodd"></path></svg>
+                            <svg class="refine-search-icon" xmlns="http://www.w3.org/2000/svg" width="23.66" height="20" viewBox="0 0 23.66 20">
+                                <path id="target" d="M24.044,20.152A10.187,10.187,0,0,1,24.1,21.2,10,10,0,1,1,19.973,13.1l-.745,2.778a7.375,7.375,0,1,0,2.037,3.527l2.777.744ZM13.436,21.579a.764.764,0,0,0,1.045.278l6.549-3.781,2.312.619,4.414-2.549-3.356-.9.9-3.356-4.414,2.549-.619,2.312-6.551,3.782a.764.764,0,0,0-.278,1.045Zm.661-3.032a2.671,2.671,0,0,1,.518.05L17.2,17.106a5.132,5.132,0,1,0,2.03,4.089,5.173,5.173,0,0,0-.04-.641l-2.582,1.491a2.649,2.649,0,1,1-2.51-3.5Z" transform="translate(-4.097 -11.195)" fill="#00a6ed" fill-rule="evenodd"></path>
+                            </svg>
                         <?php } ?>
                         <span class="refine-search-title"><?= esc_html($refine_title) ?></span>
                     </div>
@@ -1673,14 +1675,14 @@ class Influencer_Search
                 <div class="refine-search-backdrop refine-search-trigger"></div>
                 <div class="influencer-search-filter-holder">
                     <div class="refine-search-modal-header">
+                        <?php if (! empty($refine_subtext)) { ?>
+                            <p class="refine-search-subtext refine-search-subtext--modal"><?= esc_html($refine_subtext) ?></p>
+                        <?php } ?>
                         <button type="button" class="refine-search-close refine-search-trigger" aria-label="Close">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
                             </svg>
                         </button>
-                        <?php if (! empty($refine_subtext)) { ?>
-                            <p class="refine-search-subtext refine-search-subtext--modal"><?= esc_html($refine_subtext) ?></p>
-                        <?php } ?>
                     </div>
                     <div class="influencer-search-item niche-filters required-on-search">
                         <?= self::select_filter('niche', 'Niche Filter', 'Select your niche filters', $influencer_search_fields['niche'] ?? '', 'checkbox', true) ?>
