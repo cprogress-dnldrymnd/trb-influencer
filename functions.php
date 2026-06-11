e8fdde76af706536e30c5f5c034865fc72dfd3b6
Samuel Sutton <sam@digitallydisruptive.co.uk>
2026-06-11 13:11:02 +0100

    PDF export fix

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ if (! defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-define('HELLO_ELEMENTOR_CHILD_VERSION', '2.1.2');
+define('HELLO_ELEMENTOR_CHILD_VERSION', '2.1.3');
 
 /**
  * Load child theme scripts & styles.
